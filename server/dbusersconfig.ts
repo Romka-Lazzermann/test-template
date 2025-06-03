377a3385ff1c38f6b1693107331f7ab2521932c0
Romka-Lazzermann <lazarev_full_stack@uapp.ai>
2025-06-03 16:55:44 +0300

    dotenv/config

--- server/dbusersconfig.ts
+++ server/dbusersconfig.ts
@@ -8,9 +8,9 @@ export const UserDbDataSource = new DataSource({
     type: 'mysql',
     host: process.env.DB_HOST,
     port: +(process.env.DB_ACCESS_PORT || 3306),
-    username: process.env.DB_ACCESS_USER || process.env.DB_ACCESS_USER,
-    password: process.env.DB_ACCESS_PASSWORD || process.env.DB_PASSWORD,
-    database: process.env.DB_ACCESS_NAME || process.env.DB_ACCESS_NAME,
+    username: process.env.DB_ACCESS_USER || "",
+    password: process.env.DB_ACCESS_PASSWORD || "",
+    database: process.env.DB_ACCESS_NAME || "",
     entities: [Users],
     migrations: ['src/migrations/user_db/*.ts'],
     synchronize: false
--- server/ormconfig.ts
+++ server/ormconfig.ts
@@ -1,6 +1,6 @@
 
 import {DataSource} from 'typeorm'
-import dotenv from 'dotenv'
+import 'dotenv/config';
 import path from 'path';
 import {Blog} from './src/entity/BlogModel'
 import {Category} from './src/entity/CategoryModel'
@@ -9,19 +9,14 @@ import {Style} from './src/entity/StyleModel'
 import {Combination} from './src/entity/CombinationModel'
 
 
-const credentials = dotenv.config({ path: path.resolve(__dirname, './.env') });
-console.log("credentials", credentials)
-// console.log("dirname", __dirname + "\\\src\\\entity\\\*.ts")
-// console.log("dirname", __dirname + "\\\src\\\migrations\\\*.ts")
-
 export const AppDataSource = new DataSource({
     migrationsRun: true,
     type: 'mysql',
     host: process.env.DB_HOST,
     port: +(process.env.DB_PORT || 3306),
-    username: credentials?.parsed?.DB_USER || process.env.DB_USER,
-    password: credentials?.parsed?.DB_PASS || process.env.DB_PASSWORD,
-    database: credentials?.parsed?.DB_NAME || process.env.DB_NAME,
+    database: process.env.DB_NAME || "",
+    username: process.env.DB_USER || "",
+    password: process.env.DB_PASSWORD || "",
     entities: [Blog, Category, Channel, Style, Combination],
     migrations: ["/src/migrations/*.ts"],
     synchronize: false
