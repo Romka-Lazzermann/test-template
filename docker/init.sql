c9c8ec47f7d21dc378d7e241bf1e69bdc7b5c7ea
Romka-Lazzermann <lazarev_full_stack@uapp.ai>
2025-06-03 09:38:20 +0300

    .

--- docker/init.sql
+++ docker/init.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE IF NOT EXISTS amongus;
+CREATE DATABASE IF NOT EXISTS everyexplorer;
 CREATE DATABASE IF NOT EXISTS amongus_access;
 
 CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED WITH mysql_native_password  BY 'admin';
-GRANT ALL PRIVILEGES ON amongus.* TO 'admin'@'%';
+GRANT ALL PRIVILEGES ON everyexplorer.* TO 'admin'@'%';
 
 CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED WITH mysql_native_password  BY 'admin';
-GRANT ALL PRIVILEGES ON amongus.* TO 'admin'@'%';
+GRANT ALL PRIVILEGES ON everyexplorer_access.* TO 'admin'@'%';
 
 FLUSH PRIVILEGES;
