bba46770eebbc6ba5ced5e884972300c47469471
Romka-Lazzermann <lazarev_full_stack@uapp.ai>
2025-06-03 17:11:06 +0300

    asset delete on error

--- server/src/interfaces.ts
+++ server/src/interfaces.ts
@@ -6,3 +6,7 @@ export interface LoginCredentials {
     login : string,
     password: string
 }
+
+export interface MulterRequest extends Request {
+  file: Express.Multer.File; 
+}
--- server/src/routes/blog.ts
+++ server/src/routes/blog.ts
@@ -2,12 +2,16 @@ import { Router } from 'express';
 import { BlogService } from '../services/BlogService';
 import { Request, Response } from 'express';
 import { upload } from '../middlewares/uploadMiddleware';
+import { MulterRequest } from '../interfaces'
+import fs from 'fs';
+import path from 'path';
+
 const router = Router();
 
 export const createBlogRoutes = (dataSource) => {
   const blogService = new BlogService(dataSource);
   
-  router.post('/', upload.single('img'), async (req , res ) => {
+  router.post('/', upload.single('img'), async (req : MulterRequest | Request, res ) => {
     try {
       const { title, description, category_id, status } = req.body;
       const blog = await blogService.create({
@@ -15,10 +19,18 @@ export const createBlogRoutes = (dataSource) => {
         description,
         category_id,
         status,
-        img: req.file ? req.file.path : null
+        img: req.file ? req.file.path : ""
       });
       res.status(201).json(blog);
     } catch (err) {
+      if(req?.file){
+        const filepath = path.join(__dirname, '../..', 'assets', req.file.filename)
+        fs.unlink(filepath, (unlinkErr) => {
+            if(unlinkErr){
+              console.error("Error while deleting file")
+            }
+        })
+      }
       console.log("Error message : ", err.message)
       res.status(400).json({ success: 0, error: "Error while creating a blog" });
     }
