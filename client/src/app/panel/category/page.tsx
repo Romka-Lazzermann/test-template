205f2e12b3c9c70967328bc906afc3a9a102b7dc
Romka-Lazzermann <lazarev_full_stack@uapp.ai>
2025-06-04 18:59:34 +0300

    .

--- client/src/app/panel/category/page.tsx
+++ client/src/app/panel/category/page.tsx
@@ -2,7 +2,7 @@ import AdminLayout from '@/components/AdminLayout';
 import CategoryTable from '@/components/panel/category/CategoryTable';
 export default function BlogPage() {
     return (
-        <AdminLayout title="Blog">
+        <AdminLayout title="Category">
             <div className={`container`}>
                 <CategoryTable />
             </div>
