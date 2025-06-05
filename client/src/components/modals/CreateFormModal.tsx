0979540afccd640c9be497e49d969ad94b91b72a
Romka-Lazzermann <lazarev_full_stack@uapp.ai>
2025-06-05 12:53:55 +0300

    additional on close callback

--- client/src/components/modals/CreateFormModal.tsx
+++ client/src/components/modals/CreateFormModal.tsx
@@ -4,15 +4,19 @@ import styles from '@/components/index.module.css'
 interface ModalProps {
     show: boolean;
     setShow: (value: boolean) => void;
+    handleCloseCallback?: () => void;
     children: React.ReactNode;
 }
 
-const CreateFormModal: React.FC<ModalProps> = ({ show, setShow, children }) => {
+const CreateFormModal: React.FC<ModalProps> = ({ show, setShow, children, handleCloseCallback }) => {
     if (!show) return null;
 
     const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement>) => {
         if (e.target === e.currentTarget) {
             setShow(false);
+            if(handleCloseCallback){
+                handleCloseCallback();
+            }
         }
     };
 
