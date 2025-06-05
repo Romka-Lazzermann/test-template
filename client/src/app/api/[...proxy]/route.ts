3d070ea783bd9236ba077a4344b414916a7795fc
Romka-Lazzermann <lazarev_full_stack@uapp.ai>
2025-06-05 12:53:09 +0300

    put handler

--- client/src/app/api/[...proxy]/route.ts
+++ client/src/app/api/[...proxy]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import {serialize} from 'cookie'
+import { serialize } from 'cookie'
 export async function GET(request: NextRequest) {
   const { pathname, search } = new URL(request.url);
   const targetPath = pathname.replace(/^\/api/, '');
@@ -70,6 +70,48 @@ export async function POST(request: NextRequest) {
       },
     });
   }
+
+  return NextResponse.json(data, {
+    status: response.status,
+    headers: response.headers,
+  });
+}
+
+export async function PUT(request: NextRequest) {
+  const { pathname, search } = new URL(request.url);
+  const targetPath = pathname.replace(/^\/api/, '');
+
+  const targetUrl = `${process.env.SERVER_URL}${targetPath}${search}`;
+  const headers = new Headers(request.headers);
+
+  let body: any = null;
+  let _set_headers = true;
+  const contentType = headers.get('content-type') || '';
+  if (contentType.includes('application/json')) {
+    const json = await request.json();
+    body = JSON.stringify(json);
+  }
+  else if (contentType.includes('multipart/form-data')) {
+    body = await request.formData();
+    _set_headers = false;
+  }
+  else {
+    body = await request.body;
+    _set_headers = false
+  }
+  const request_options = _set_headers ? {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  } : {}
+  const response = await fetch(targetUrl, {
+    method: 'PUT',
+    body,
+    ...request_options
+  });
+
+  const data = await response.json();
+  console.log("data", data)
 
   return NextResponse.json(data, {
     status: response.status,
